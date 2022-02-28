52d559dc52397e43c835c34dbe8bac3afed7b621
Touhidulislam430590 <sanon.touhid980@gmail.com>
2022-02-28 22:56:32 +0600

    complete all phones display in front page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 fetch('https://openapi.programming-hero.com/api/phones')
 .then(Response => Response.json())
-.then(data => console.log(data));
+.then(data => loadApi(data.data));
+
+const item = item => console.log(item);
+
+const loadApi = (data) => {
+    // const individualPhone = data.map(item);
+    const parentDiv = document.getElementById('parentDiv');
+    for (const item of data) {
+        console.log(item);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card p-3">
+            <div class="d-flex justify-content-center align-items-center">
+                <img src="${item.image}" class="card-img-top img-fluid product-image">
+            </div>
+            
+            <div class="card-body">
+                <h5 class="card-title">${item.phone_name}</h5>
+                <p class="card-text">${item.brand}</p>
+            </div>
+        </div>
+        `;
+        parentDiv.appendChild(div);
+    }
+}
